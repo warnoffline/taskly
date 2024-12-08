1ad74622ac2c19d202ac560e47484b4b84835784
warnoffline <hametshin543@gmail.com>
2024-12-09 00:12:46 +0300

    update fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
