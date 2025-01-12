5db4c04d52763a26bf3fce832cd2ece252adc28e
warnoffline <hametshin543@gmail.com>
2025-01-13 00:35:28 +0300

    fix double notification

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -24,14 +24,3 @@ function App() {
 }
 
 export default App;
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/firebase-messaging-sw.js')
-    .then(function (registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function (error) {
-      console.log('Service Worker registration failed:', error);
-    });
-}
