6990ab976b8288af44e962d812192d589c112b0c
warnoffline <hametshin543@gmail.com>
2025-01-15 13:06:51 +0300

    update icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,14 +15,16 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
